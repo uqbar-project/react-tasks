a44240c101288c5794ed953f7430b32d34de15e5
Nicolás Passerini <npasserini@gmail.com>
2020-11-13 20:04:01 -0300

    Task component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+const tasks = [
+  { id: 1, text: "Armar charla de React" },
+  { id: 2, text: "Pasarla a inglés o francés" },
+  { id: 3, text: "No comprometerse a dar charlas en el futuro" }
+]
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {tasks.map(task => <Task id={task.id} task={task} />)}
     </div>
   );
+}
+
+function Task({ task }) {
+  return (<div className="Task">{task.text}</div>)
 }
 
 export default App;
