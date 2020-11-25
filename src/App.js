996b5c2f8810ec4f17ede1fa4b4f241c10376196
Nicolás Passerini <npasserini@gmail.com>
2020-11-25 19:45:56 -0300

    v0

--- src/App.js
+++ src/App.js
@@ -1,47 +1,19 @@
 import React from "react"
 
-import { createTask, loadTasks } from './tasks.js'
-import { TaskList, AddTaskForm } from './TaskList.js'
 import './App.css'
 
+const tasks = [
+  { id: 1, text: "Armar charla de React" },
+  { id: 2, text: "Pasarla a inglés o francés" },
+  { id: 3, text: "No comprometerse a dar charlas en el futuro" }
+]
+
 class App extends React.Component {
-  state = { filterText: '' }
-
-  async componentDidMount() {
-    const tasks = await loadTasks()
-    this.setState({ tasks })
-  }
-
-  addTask = async (taskDescription) => {
-    const newTask = await createTask(taskDescription)
-    this.setState(({ tasks }) => ({ tasks: [...tasks, newTask] }))
-  }
-
-  deleteTask = (task) => {
-    await deleteTask(task.id)
-    this.setState({
-      tasks: this.state.tasks.filter(({ id }) => task.id !== id)
-    })
-  }
-
-  changeFilter = (filterText) => {
-    this.setState({ filterText })
-  }
-
-  getVisibleTasks = () => this.state.tasks.filter(task => task.text.includes(this.state.filterText))
-
   render() {
-    return (this.state.tasks
-      ? <div className="App">
-        <TaskList
-          tasks={this.getVisibleTasks()}
-          onChangeFilter={this.changeFilter}
-          onDeleteTask={this.deleteTask}
-        />
-        <AddTaskForm onAddTask={this.addTask} />
+    return (
+      <div className="App">
       </div>
-      : <div className="Loader" />
-    );
+    )
   }
 }
 
